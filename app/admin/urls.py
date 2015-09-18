f3e936ebc7921c3ff0b020f676aacebddc2b6372
Dirk Moors <dirkmoors@gmail.com>
2015-09-18 15:38:39 +0200

    Added default redirect

--- app/admin/urls.py
+++ app/admin/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+from django.core.urlresolvers import reverse_lazy
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^show/', 'kong_admin.views.show_config')
+    url(r'^admin/', include(admin.site.urls), name='admin'),
+
+    # Shows entire configuration
+    url(r'^show/', 'kong_admin.views.show_config'),
+
+    # Default
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('admin'), permanent=False)),
 ]
